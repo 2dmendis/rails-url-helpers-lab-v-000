8eda2cc2702883025b7d835446eddf066174c398
Dmitri Mendis <2dmendis@gmail.com>
2018-07-12 22:54:02 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,6 +3,7 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
+      add_column :active, :boolean, :default => true
 
       t.timestamps null: false
     end
